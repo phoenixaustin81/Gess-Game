77fd2fc01bde8c95512ab58c81413cc3b097f476
phoenixaustin81 <harrisph@oregonstate.edu>
2020-05-26 12:50:58 -0500

    Portfolio Project

--- GessGame.py
+++ GessGame.py
@@ -11,6 +11,37 @@ class GessGame:
     def get_board(self):
         return self._board
 
+    def make_move(self, start, end):
+
+        # convert game-board positions to list indices
+        start = self.coordinate_conversion(start)
+        end = self.coordinate_conversion(end)
+
+        # if the game is over
+        if self._game_state != "UNFINISHED":
+            return False
+
+        # if the move is not on the board
+        if start[0] > 19 or start [0] < 1 or start[1] > 20 or start[1] < 1:
+            return False
+        if end[0] > 19 or end[0] < 1 or end[1] > 20 or end[1] < 1:
+            return False
+
+        # if the piece has stones of the opponent
+        check_piece = Piece(start, self._board.get_game_board())
+        if not check_piece.valid_piece(self._up_next):
+            return False
+
+    def coordinate_conversion(self, position):
+        """
+        Converts a game-board position to a list with corresponding indices list[x][x]
+        Example: 'd16' to  [4,5], which is list[4][5]
+        """
+        return [ord(position[0]) - 96, 21 - int(position[1:])]
+
+    def valid_direction(self, start, end):
+        piece_start = Piece(start, self._board.get_game_board())
+        piece_end = Piece(end, self._board.get_game_board())
 
 class Board:
 
@@ -55,9 +86,11 @@ class Board:
         self._board[center_coordinates[0]][center_coordinates[1]-1] = ' '
 
     def add_piece(self, piece, location):
+        # if the center of the piece is being added to a valid spot on the board...
         if location[0] in range(2,20) and location[1] in range(2,20):
             self._board[location[0]][location[1]] = piece.get_piece_center()
 
+            # then add the center, plus any part of the piece that is not off the board
             if location[0] - 1 in range(2,20) and location[1] in range(2,20):
                 self._board[location[0] - 1][location[1]] = piece.get_piece_N()
 
@@ -83,9 +116,6 @@ class Board:
                 self._board[location[0]][location[1] - 1] = piece.get_piece_W()
 
 
-
-
-
 class Piece:
 
     def __init__(self, middle, board):
@@ -108,6 +138,20 @@ class Piece:
 
     def get_piece_matrix(self):
         return self._piece_matrix
+
+    def valid_piece(self, up_next):
+        """
+        Confirms that a piece does not have stones of the other player
+        """
+        if self._center == up_next:
+            return False
+
+        if up_next in self.perimeter():
+                print('False')
+                return False
+
+        print('True')
+        return True
 
     def get_piece_center(self):
         return self._center
@@ -136,6 +180,7 @@ class Piece:
     def get_piece_E(self):
         return self._E
 
+
 def display_board():
     for row in test.get_board().get_game_board():
         print(row)
@@ -152,3 +197,5 @@ footprint = Piece([3,3], test.get_board().get_game_board())
 display_piece()
 test.get_board().add_piece(footprint, [13,11])
 display_board()
+
+print(test.make_move('k6', 'i3'))
